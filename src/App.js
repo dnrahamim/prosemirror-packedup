dc27a28bbc08259324ad2806433b0d67a7c26070
Dorian Rahamim <dnrahamim@gmail.com>
2018-11-01 14:07:59 -0400

    reverts to original create-react-app situation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-
-const {Schema, DOMParser} = require("prosemirror-model")
-const {EditorView, Decoration, DecorationSet} = require("prosemirror-view")
-const {EditorState, TextSelection, Plugin} = require("prosemirror-state")
-const {schema} = require("prosemirror-schema-basic")
-const {addListNodes} = require("prosemirror-schema-list")
-const {exampleSetup} = require("prosemirror-example-setup")
-
-/*
-EditorView = window.EditorView
-state = window.state
-new EditorView(document.querySelector(".full"), {state})
-*/
+import './App.css';
 
 class App extends Component {
-
-  componentDidMount() {
-    const demoSchema = new Schema({
-      nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-      marks: schema.spec.marks
-    })
-    
-    let state = EditorState.create({doc: DOMParser.fromSchema(demoSchema).parse(document.querySelector("#content")),
-          plugins: exampleSetup({schema: demoSchema})})
-    
-    window.view = new EditorView(document.querySelector("#menu"), {state})
-  }
-
   render() {
-    return ([
-      <div key='menu' id="menu"></div>,
-      <div key='content' id="content"></div>
-    ]);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
